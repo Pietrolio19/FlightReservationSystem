108b253d6513ff33566927e88c80a74492b34a9c
Pietro Madioni <126822389+Pietrolio19@users.noreply.github.com>
2026-03-21 19:04:46 +0100

    Finita UI per conferma dati.

--- src/main/java/UI/controller/flight/ConfirmationController.java
+++ src/main/java/UI/controller/flight/ConfirmationController.java
@@ -1,105 +1,126 @@
 package UI.controller.flight;
 
-import UI.navigator.Navigator;
-import UI.navigator.NavigatorAware;
 import domain.flight.Seat;
+import domain.reservation.SeatReservation;
 import domain.user.Passenger;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.*;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-import service.BookingService;
 import util.session.BookingSession;
 
-import java.util.*;
+import java.time.format.DateTimeFormatter;
 
-public class PassengerController implements NavigatorAware {
+public class ConfirmationController {
     //attributi
-    private Navigator navigator;
-    private final Map<String, Map<String, Control>> cardFields = new HashMap<>();
-    private final BookingService bookingService = new BookingService();
-    private final Map<String, Passenger> passengersBySeatCode = new LinkedHashMap<>();
     private final BookingSession session = BookingSession.getInstance();
 
     @FXML
     private VBox passengerCardsArea;
 
     @FXML
-    private Button continueButton;
+    private Label airlineLabel;
+
+    @FXML
+    private Label flightCodeLabel;
+
+    @FXML
+    private Label departureCityLabel;
+
+    @FXML
+    private Label departureIataLabel;
+
+    @FXML
+    private Label departureTimeLabel;
+
+    @FXML
+    private Label durationLabel;
+
+    @FXML
+    private Label arrivalCityLabel;
+
+    @FXML
+    private Label arrivalIataLabel;
+
+    @FXML
+    private Label arrivalTimeLabel;
+
+    @FXML
+    private Label departureDateLabel;
 
     @FXML
     private void initialize() {
+        setFlightData();
         createPassengerCardList();
-        continueButton.setOnAction(e -> continueToConfirmView());
+    }
+
+    private void setFlightData() {
+        airlineLabel.setText(session.getSelectedFlight().getAirline().getName());
+        flightCodeLabel.setText(session.getSelectedFlight().getFlightCode());
+        departureCityLabel.setText(session.getSelectedFlight().getDeparture().getCity());
+        departureIataLabel.setText(session.getSelectedFlight().getDeparture().getIata());
+        departureDateLabel.setText(session.getSelectedFlight().getDepartureDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        departureTimeLabel.setText(session.getSelectedFlight().getDepartureTime().toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm")));
+        durationLabel.setText(session.getSelectedFlight().formattedDuration());
+        arrivalCityLabel.setText(session.getSelectedFlight().getArrival().getCity());
+        arrivalIataLabel.setText(session.getSelectedFlight().getArrival().getIata());
+        arrivalTimeLabel.setText(session.getSelectedFlight().getArrivalTime().toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm")));
     }
 
     private void createPassengerCardList() {
-        for(Seat s: session.getSelectedSeats()) {
-            passengerCardsArea.getChildren().add(createPassengerCard(s));
+        for(SeatReservation sr: session.getSeatReservations()) {
+            passengerCardsArea.getChildren().add(createPassengerCard(sr.getSeat(), sr.getPassenger()));
         }
     }
 
-    private Node createPassengerCard(Seat seat) {
-        Map<String, Control> fields = new HashMap<>();
-
+    private Node createPassengerCard(Seat seat, Passenger passenger) {
         Label seatLabel = new Label("Posto " + seat.getSeatCode());
 
         Label nameLabel = new Label("Nome:");
-        TextField nameField = new TextField();
+        Label nameField = new Label(passenger.getName());
         HBox nameBox = new HBox(10, nameLabel, nameField);
-        fields.put("name", nameField);
 
         Label surnameLabel = new Label("Cognome:");
-        TextField surnameField = new TextField();
+        Label surnameField = new Label(passenger.getSurname());
         HBox surnameBox = new HBox(10, surnameLabel, surnameField);
-        fields.put("surname", surnameField);
 
         Label birthDateLabel = new Label("Data di Nascita:");
-        DatePicker birthDateField  = new DatePicker();
-        birthDateField.setPromptText("Scegli data");
+        Label birthDateField  = new Label(passenger.getDateOfBirth().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
         HBox birthDateBox = new HBox(10, birthDateLabel, birthDateField);
-        fields.put("birthDate", birthDateField);
 
         HBox firstRow = new HBox(100, nameBox, surnameBox, birthDateBox);
 
         Label addressLabel = new Label("Indirizzo:");
-        TextField addressField = new TextField();
+        Label addressField = new Label(passenger.getAddress());
         HBox secondRow = new HBox(10, addressLabel, addressField);
-        fields.put("address", addressField);
 
         Label cityLabel = new Label("Città:");
-        TextField cityField = new TextField();
+        Label cityField = new Label(passenger.getCity());
         HBox cityBox = new HBox(10, cityLabel, cityField);
-        fields.put("city", cityField);
 
         Label provinceLabel = new Label("Provincia:");
-        TextField provinceField = new TextField();
+        Label provinceField = new Label(passenger.getProvince());
         HBox provinceBox = new HBox(10, provinceLabel, provinceField);
-        fields.put("province", provinceField);
 
         Label countryLabel = new Label("Stato:");
-        TextField countryField = new TextField();
+        Label countryField = new Label(passenger.getCountry());
         HBox countryBox = new HBox(10, countryLabel, countryField);
-        fields.put("country", countryField);
 
         HBox thirdRow = new HBox(100, cityBox, provinceBox, countryBox);
 
         Label codFiscLabel = new Label("Codice Fiscale:");
-        TextField codFiscField = new TextField();
+        Label codFiscField = new Label(passenger.getCodFisc());
         HBox codFiscBox = new HBox(10, codFiscLabel, codFiscField);
-        fields.put("codFisc", codFiscField);
 
         Label codIdLabel = new Label("Codice Carta Identità:");
-        TextField codIdField = new TextField();
+        Label codIdField = new Label(passenger.getCodId());
         HBox codIdBox = new HBox(10, codIdLabel, codIdField);
-        fields.put("codId", codIdField);
 
         Label phoneLabel = new Label("Cellulare:");
-        TextField phoneField = new TextField();
+        Label phoneField = new Label(passenger.getPhoneNumber());
         HBox phoneBox = new HBox(10, phoneLabel, phoneField);
-        fields.put("phone", phoneField);
 
         HBox lastRow = new HBox(100, codFiscBox, codIdBox, phoneBox);
 
@@ -131,43 +152,6 @@ public class PassengerController implements NavigatorAware {
 
         card.getStyleClass().add("passenger-card-wrapper");
 
-        //Mappatura per recupero dati
-        cardFields.put(seat.getSeatCode(), fields);
-
         return card;
-    }
-
-    private void continueToConfirmView(){
-        savePassengersData();
-        bookingService.createSeatReservations(passengersBySeatCode);
-        navigator.loadView("confirm-view.fxml");
-    }
-
-    private void savePassengersData() {
-        for(Map.Entry<String, Map<String, Control>> entry: cardFields.entrySet()){
-            String seatCode = entry.getKey();
-            Map<String, Control> fields = entry.getValue();
-
-            Passenger passenger = new Passenger();
-            passenger.setName(((TextField) fields.get("name")).getText());
-            passenger.setSurname(((TextField) fields.get("surname")).getText());
-            passenger.setDateOfBirth(((DatePicker) fields.get("birthDate")).getValue());
-            passenger.setAddress(((TextField) fields.get("address")).getText());
-            passenger.setCity(((TextField) fields.get("city")).getText());
-            passenger.setProvince(((TextField) fields.get("province")).getText());
-            passenger.setCountry(((TextField) fields.get("country")).getText());
-            passenger.setCodFisc(((TextField) fields.get("codFisc")).getText());
-            passenger.setCodId(((TextField) fields.get("codId")).getText());
-            passenger.setPhoneNumber(((TextField) fields.get("phone")).getText());
-
-            passengersBySeatCode.put(seatCode, passenger);
-        }
-        bookingService.savePassengers(passengersBySeatCode);
-    }
-
-
-    @Override
-    public void setNavigator(Navigator navigator) {
-        this.navigator = navigator;
     }
 }
